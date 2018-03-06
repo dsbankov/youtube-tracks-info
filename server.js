859740967139aad2a9280e4c0c95ba74edf851e7
dsbankov <dsbankov@gmail.com>
2018-03-06 08:02:21 +0200

    parse query string

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.set('port', (process.env.PORT || 5000));
 var client_id = '34cc976bb02444f598495f72da4e5615'; // TODO move to config file
 var client_secret = '6203a8869f664f57b294c1a02d904a01'; // TODO move to config file
 
-// your application requests authorization
 var authOptions = {
 	url: 'https://accounts.spotify.com/api/token',
 	headers: {
@@ -38,7 +37,7 @@ app.use(function (req, res, next) {
 app.get('/trackAnalysis', function (req, res) {
 	var queryString = req.query.q;
 	if (!queryString) {
-		res.status(400).end('No track name given. Use /trackAnalysys?q=' + track_name + '.');
+		res.status(400).end('No track name given. Use /trackAnalysys?q=<track_name>.');
 		return;
 	}
 	request.post(authOptions, function (error, response, body) {
@@ -46,9 +45,11 @@ app.get('/trackAnalysis', function (req, res) {
 			// use the access token to access the Spotify Web API
 			var token = body.access_token;
 			console.log('Access token: ' + token);
+			
+			var parsedQueryString = parse(queryString);
 
 			request.get({
-				url: 'https://api.spotify.com/v1/search?q=' + queryString + '&type=track',
+				url: 'https://api.spotify.com/v1/search?q=' + parsedQueryString + '&type=track',
 				headers: {
 					'Authorization': 'Bearer ' + token
 				},
@@ -57,8 +58,9 @@ app.get('/trackAnalysis', function (req, res) {
 
 				var items = body.tracks.items;
 				console.log(items.length + ' results found.');
+				
 				if (items.length <= 0) {
-					res.status(404).end('No tracks with name "' + queryString + '" found.');
+					res.status(404).end('No tracks with name "' + parsedQueryString + '" found.');
 					return;
 				}
 				var track = items[0];
@@ -96,7 +98,18 @@ app.get('/trackAnalysis', function (req, res) {
 
 		}
 	});
-})
+});
+
+function parse(queryString) {
+	var parsedQueryString = queryString;
+	if (parsedQueryString.match((/(\(|\[).*remix.*(\)|\])/gi))) { // if we have (..remix...) / [..remix..] we should not delete everything
+		parsedQueryString = parsedQueryString.replace(/(\(|\[|\)|\])/gi, ""); // remove the brackets only
+	} else {
+		parsedQueryString = parsedQueryString.replace(/(\(|\[).*(\)|\])/gi, ""); // remove everything between brackets (...) or [...]
+	}
+	parsedQueryString = parsedQueryString.replace(/4k/gi, ""); // remove 4K 
+	return parsedQueryString.trim();
+}
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
