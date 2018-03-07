9e2e0fe8c0d43d387c97c1a3814b3078cdf94978
dsbankov <dsbankov@gmail.com>
2018-03-07 08:03:25 +0200

    refactoring

--- server.js
+++ server.js
@@ -1,35 +1,23 @@
 var express = require('express');
 var request = require('request');
 
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
 var client_id = '34cc976bb02444f598495f72da4e5615'; // TODO move to config file
 var client_secret = '6203a8869f664f57b294c1a02d904a01'; // TODO move to config file
 
-var authOptions = {
-	url: 'https://accounts.spotify.com/api/token',
-	headers: {
-		'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-	},
-	form: {
-		grant_type: 'client_credentials'
-	},
-	json: true
-};
+var app = express();
+app.set('port', (process.env.PORT || 5000));
 
 // Add headers
-app.use(function (req, res, next) {
+app.use(function (request, response, next) {
 	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'https://www.youtube.com');
+	response.setHeader('Access-Control-Allow-Origin', 'https://www.youtube.com');
 	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	// Set to true if you need the website to include cookies in the requests sent
 	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
+	response.setHeader('Access-Control-Allow-Credentials', true);
 	// Pass to next layer of middleware
 	next();
 });
@@ -40,77 +28,95 @@ app.get('/trackAnalysis', function (req, res) {
 		res.status(400).end('No track name given. Use /trackAnalysys?q=<track_name>.');
 		return;
 	}
-	request.post(authOptions, function (error, response, body) {
+	request.post(getAuthOptions(client_id, client_secret), function (error, response, body) {
 		if (!error && response.statusCode === 200) {
 			// use the access token to access the Spotify Web API
 			var token = body.access_token;
+			var parsed_query_string = parseQueryString(queryString);
 			console.log('Access token: ' + token);
+			console.log('Prased query string: ' + parsed_query_string);
 			
-			var parsedQueryString = parse(queryString);
-
-			request.get({
-				url: 'https://api.spotify.com/v1/search?q=' + parsedQueryString + '&type=track',
-				headers: {
-					'Authorization': 'Bearer ' + token
-				},
-				json: true
-			}, function (error, response, body) {
+			request.get(getSearchOptions(parsed_query_string, token), function (error, response, body) {
 
 				var items = body.tracks.items;
 				console.log(items.length + ' results found.');
 				
 				if (items.length <= 0) {
-					res.status(404).end('No tracks with name "' + parsedQueryString + '" found.');
+					res.status(404).end('No tracks with name "' + parsed_query_string + '" found.');
 					return;
 				}
 				var track = items[0];
+				var artists_names = getArtistsNames(track);
 				
-				var artists = [];
-				track.artists.forEach(function(artist) {
-					artists.push(artist.name);
-				});
-				
-				console.log('Artist: ' + artists.join());
-				console.log('Track: ' + track.name);
-				console.log('ID: ' + track.id);
-				var response_object = {};
-				response_object.artist = artists.join();
-				response_object.track = track.name;
-				response_object.id = track.id;
+				var track_info = {};
+				track_info.artists = artists_names;
+				track_info.track = track.name;
+				track_info.id = track.id;
 
-				request.get({
-					url: 'https://api.spotify.com/v1/audio-features/' + track.id,
-					headers: {
-						'Authorization': 'Bearer ' + token
-					},
-					json: true
-				}, function (error, response, body) {
-					console.log('Key: ' + body.key);
-					console.log('Tempo: ' + body.tempo);
-					console.log('Loudness: ' + body.loudness);
-					response_object.key = body.key;
-					response_object.tempo = body.tempo;
-					response_object.loudness = body.loudness;
-					res.send(response_object);
+				request.get(getTrackFeaturesOptions(track.id, token), function (error, response, body) {
+					track_info.key = body.key;
+					track_info.tempo = body.tempo;
+					track_info.loudness = body.loudness;
+					console.log(track_info);
+					res.send(track_info);
 				});
-
 			});
-
 		}
 	});
 });
 
-function parse(queryString) {
-	var parsedQueryString = queryString;
-	if (parsedQueryString.match((/(\(|\[).*remix.*(\)|\])/gi))) { // if we have (..remix...) / [..remix..] we should not delete everything
-		parsedQueryString = parsedQueryString.replace(/(\(|\[|\)|\])/gi, ""); // remove the brackets only
-	} else {
-		parsedQueryString = parsedQueryString.replace(/(\(|\[).*(\)|\])/gi, ""); // remove everything between brackets (...) or [...]
-	}
-	parsedQueryString = parsedQueryString.replace(/(4k|hd|ft|ft.|feat|feat.)/gi, ""); // remove 4K
-	return parsedQueryString.trim();
-}
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function getAuthOptions(client_id, client_secret) {
+	return {
+		url: 'https://accounts.spotify.com/api/token',
+		headers: {
+			'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+		},
+		form: {
+			grant_type: 'client_credentials'
+		},
+		json: true
+	};
+}
+
+function getSearchOptions(parsed_query_string, token) {
+	return {
+		url: 'https://api.spotify.com/v1/search?q=' + parsed_query_string + '&type=track',
+		headers: {
+			'Authorization': 'Bearer ' + token
+		},
+		json: true
+	};
+}
+
+function getTrackFeaturesOptions(track_id, token) {
+	return {
+		url: 'https://api.spotify.com/v1/audio-features/' + track_id,
+		headers: {
+			'Authorization': 'Bearer ' + token
+		},
+		json: true
+	};
+}
+
+function getArtistsNames(track) {
+	var artists = [];
+	track.artists.forEach(function(artist) {
+		artists.push(artist.name);
+	});
+	return artists.join();
+}
+
+function parseQueryString(queryString) {
+	var parsed_query_string = queryString;
+	if (parsed_query_string.match((/(\(|\[).*remix.*(\)|\])/gi))) { // if we have (..remix...) / [..remix..] we should not remove everything
+		parsed_query_string = parsed_query_string.replace(/(\(|\[|\)|\])/gi, ""); // remove the brackets only
+	} else {
+		parsed_query_string = parsed_query_string.replace(/(\(|\[).*(\)|\])/gi, ""); // remove everything between brackets (...) or [...]
+	}
+	parsed_query_string = parsed_query_string.replace(/(4k|hd|ft|ft.|feat|feat.)/gi, ""); // remove specific words
+	return parsed_query_string.trim();
+}
