db4e304acc26925efe6ba55371efa93190854028
dsbankov <dsbankov@gmail.com>
2018-03-13 20:36:16 +0200

    remove special characters in script

--- Youtube Tracks Info Key, BPM & RMS.user.js
+++ Youtube Tracks Info Key, BPM & RMS.user.js
@@ -2,12 +2,12 @@
 // @name         Youtube Tracks Info: Key, BPM & RMS
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.youtube.com/watch?*
-// @match        http://www.youtube.com/watch?*
+// @description  Displays the key, BPM & RMS of a track on youtube.
+// @author       dsbankov
+// @match        https://www.youtube.com/*
+// @match        http://www.youtube.com/*
 // @grant        none
-// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 $(document).ready(load_track_info); // first time load
@@ -18,29 +18,31 @@ document.addEventListener('DOMContentLoaded', load_track_info); // one-time earl
 loading_info = false;
 
 function load_track_info() {
-    console.log("Youtube Tracks Info @ " + location.pathname);
+    log("Youtube Tracks Info @ " + location.pathname);
     if (location.pathname === "/watch") {
-        console.log("Loading track info...");
+        log("Loading track info...");
         $('#track_info').remove();
         setTimeout(function() {
             var video_title_container = $("#container > h1");
             $(video_title_container).ready(function() {
                 if (video_title_container && video_title_container.length > 0 && video_title_container[0].innerText) {
                     var video_title = video_title_container[0].innerText;
-                    console.log("Searching for track by video title: " + video_title);
+                    var parsed_video_title = remove_non_word_characters(video_title);
+                    log("Searching for track by video title: " + parsed_video_title);
                     if (!loading_info) {
                         loading_info = true;
                         $.ajax({
-                            url: 'https://sleepy-beach-89012.herokuapp.com/trackAnalysis?q=' + video_title
+                            url: 'https://sleepy-beach-89012.herokuapp.com/trackAnalysis?q=' + parsed_video_title
                         }).done(function (response) {
-                            console.log("Track found: " + JSON.stringify(response));
+                            log("Track found: " + JSON.stringify(response));
                             var key = response.key;
                             var loudness = response.loudness;
                             var tempo = response.tempo;
-                            $('h1').parent().append('<br/><h2 id="track_info" style="text-decoration: none; font-weight: normal; text-align: right">Key of ' + key + ', ' + tempo + ' BPM, ' + loudness + ' RMS' + '</h2>');
+                            // $('h1').parent().append('<br/><h2 id="track_info" style="text-decoration: none; font-weight: normal; text-align: right">Key of ' + key + ', ' + tempo + ' BPM, ' + loudness + ' RMS' + '</h2>');
+                            $('#count > yt-view-count-renderer').append('<h5 id="track_info" style="color: green; text-decoration: none; font-weight: normal; text-align: right; font-style: italic;">Key of ' + key + ', ' + tempo + ' BPM, ' + loudness + ' RMS' + '</h4>');
                             loading_info = false;
-                        }).fail(function(response){
-                            console.log("Track not found: " + JSON.stringify(response));
+                        }).fail(function(response) {
+                            log("Track not found: " + JSON.stringify(response));
                             loading_info = false;
                         });
                     }
@@ -48,4 +50,18 @@ function load_track_info() {
             });
         }, 1000);
     }
+}
+
+function remove_non_word_characters(video_title) {
+    var ws_marker = "999ABCD000";
+    var result = video_title;
+    result = result.replace(/\s+/gi, ws_marker); // replace all consecutive whitespace characters with the unique marker
+    result = result.replace(/\W+/gi, ""); // remove all non-word characters (&*()_=\..., this includes the whitespaces -> that's why we replace them with the marker beforehand)
+    result = result.replace(new RegExp(ws_marker, "gi"), " "); // bring back the whitespaces
+    return result;
+    // return video_title.replace(/\s+/gi, ws_marker).replace(/\W+/gi, "").replace(new RegExp(ws_marker, "gi"), " ");
+}
+
+function log(msg) {
+    console.log("YTTI: " + msg);
 }
